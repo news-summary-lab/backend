1b712a9451569a0fb16a3c828ac5ab17d50183a1
leppard204 <eyela1004@naver.com>
2025-04-11 21:48:23 +0900

    Revert "test2"
    This reverts commit 370e92a1a42804d75f1c59c52101b87213b09598.

--- RestApi/src/main/java/com/restapi/SummaryService.java
+++ RestApi/src/main/java/com/restapi/SummaryService.java
@@ -1,34 +1,15 @@
 package com.restapi;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-@Service 
-public class SummaryService { 
-	
-	private final RestTemplate restTemplate = new RestTemplate();
-	private final RedisTemplate<String, Conversation> redisTemplate;
+  @Service public class SummaryService { 
+	  private final RestTemplate restTemplate = new RestTemplate();
   
-	public String summarize(Article article) { 
-		String fastApiUrl = "http://localhost:8000/summarize";
-		String summary = restTemplate.postForObject(fastApiUrl, article, String.class);
-		Conversation conv = new Conversation(article.getText(), summary);
-        redisTemplate.opsForList().rightPush("conversation", conv);
-		
-		return summary; 
-	  }
-	
-	public List<Conversation> getConversationHistory() {
-	    return redisTemplate.opsForList().range("conversation", 0, -1);
-	}
+	  public String summarize(Article article) { 
+		  String fastApiUrl = "http://localhost:8000/summarize"; 
+		  return restTemplate.postForObject(fastApiUrl, article, String.class); 
+	  } 
   }
  
